4066f206821b74508116648f4cf2aba4cd09c556
JacknS2520 <jackelly0221@gmail.com>
2025-05-08 08:13:06 -0500

    eddited

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener("click", mobileMenu);
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
 
-function mobileMenu() {
-hamburger.classList.toggle("active");
-navMenu.classList.toggle("active");
-}
 const navLink = document.querySelectorAll(".nav-link");
 
 navLink.forEach(n => n.addEventListener("click", closeMenu));
